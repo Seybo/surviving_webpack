ba020a7958f71f9b1be400a5fb18a26a1f1abdf7
seybo <michaelatworkspace@gmail.com>
2020-06-20 17:09:33 +0530

    add webpack-merge and update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,31 @@
+const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// Import the plugin:
-var DashboardPlugin = require("webpack-dashboard/plugin");
+const parts = require("./webpack.parts");
 
-module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack demo",
-    }),
-    new DashboardPlugin()
-  ],
-  devServer: {
-    // Display only errors to reduce the amount of output.
-    stats: "errors-only",
-
-    // Parse host and port from env to allow customization.
-    //
-    // If you use Docker, Vagrant or Cloud9, set
-    // host: "0.0.0.0";
-    //
-    // 0.0.0.0 is available to all network devices
-    // unlike default `localhost`.
-    host: process.env.HOST, // Defaults to `localhost`
-    port: process.env.PORT, // Defaults to 8080
-    open: false, // Open the page in browser
-    overlay: true,
+const commonConfig = merge([
+  {
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Webpack demo",
+      }),
+    ],
   },
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+]);
+
+module.exports = mode => {
+  if (mode === "production") {
+    return merge(commonConfig, productionConfig, { mode });
+  }
+
+  return merge(commonConfig, developmentConfig, { mode });
 };
