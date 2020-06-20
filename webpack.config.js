dfc74ce2ecdf916b78bb37da6f9965eccbf38f5c
seybo <michaelatworkspace@gmail.com>
2020-06-20 16:44:25 +0530

    add webpack-dev-server, nodemon and dashboard

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+// Import the plugin:
+var DashboardPlugin = require("webpack-dashboard/plugin");
 
 module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: "Webpack demo",
     }),
+    new DashboardPlugin()
   ],
+  devServer: {
+    // Display only errors to reduce the amount of output.
+    stats: "errors-only",
+
+    // Parse host and port from env to allow customization.
+    //
+    // If you use Docker, Vagrant or Cloud9, set
+    // host: "0.0.0.0";
+    //
+    // 0.0.0.0 is available to all network devices
+    // unlike default `localhost`.
+    host: process.env.HOST, // Defaults to `localhost`
+    port: process.env.PORT, // Defaults to 8080
+    open: false, // Open the page in browser
+    overlay: true,
+  },
 };
